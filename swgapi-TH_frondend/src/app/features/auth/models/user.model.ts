da5c92c333fd08743ebdddeb0e700644ba5c61a3
Cojorlo <jorgeastm2@gmail.com>
2025-11-15 10:42:38 -0500

    Eliminando carpeta

--- swgapi-TH_frondend/src/app/features/auth/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    username: string;
-    // password is optional because we avoid storing it when persisting the user
-    password?: string;
-    role: 'admin' | 'bienestar' | 'capacitacion' | 'incentivos';
-    planId?: string;
-}
